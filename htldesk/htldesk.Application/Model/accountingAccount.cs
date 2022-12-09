75fcb844fa619fb2ecfac3ad54cb4a1894f32b90
Say My Name <mohamadaldulemi@gmail.com>
2022-12-09 10:44:39 +0100

    accountingAccount

--- htldesk/htldesk.Application/Model/accountingAccount.cs
+++ htldesk/htldesk.Application/Model/accountingAccount.cs
@@ -8,5 +8,23 @@ namespace htldesk.Application
 {
     internal class accountingAccount
     {
+
+        public accountingAccount(string name, string nachname, string email)
+        {
+            Name = name;
+            Nachname = nachname;
+            Email = email;
+        }
+#pragma warning disable CS8618
+        protected accountingAccount() { }
+#pragma warning restore CS8618
+        public string Name { get; set; }
+        public string Nachname { get; set; }
+        public string Email { get; set; }
+
+
+
+
+
     }
 }
