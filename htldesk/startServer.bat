1c1ab2827e6cd8ed79ff5ea134cf61c2237997b4
Machsiim <95721441+Machsiim@users.noreply.github.com>
2022-12-16 19:51:17 +0100

    Merged startServer and docker run

--- htldesk/startServer.bat
+++ htldesk/startServer.bat
@@ -1,3 +1,5 @@
+docker run --name mariadb -d -p 13306:3306 -e MARIADB_USER=root -e MARIADB_ROOT_PASSWORD=mariadb_root_password mariadb:10.10.2
+
 rd /S /Q .vs 2> nul
 rd /S /Q htldesk.Application/.vs 2> nul
 rd /S /Q htldesk.Application/bin 2> nul
