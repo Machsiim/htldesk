e1cc520ee3e566a988533c0358e1d4055f8791c7
Machsiim <95721441+Machsiim@users.noreply.github.com>
2022-12-18 23:16:27 +0100

    Added bootstrap god knows how

--- htldesk/htldesk.Client/src/main.js
+++ htldesk/htldesk.Client/src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -7,11 +7,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 const app = createApp(App)
 
