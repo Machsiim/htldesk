de5ca09c8c6f48b19e2a32520793cfa1480dc7b7
domdomderbigd <dominikx423@gmail.com>
2022-12-21 10:22:59 +0100

    Commit 20221221

--- htldesk/htldesk.Application/Model/User.cs
+++ htldesk/htldesk.Application/Model/User.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace htldesk.Application.Model
         {
             Username = name;
             Email = email;
-            Password = password;
+            SetPassword(password);
         }
 
         [Key]
@@ -33,5 +34,51 @@ namespace htldesk.Application.Model
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+
+
+        public string Salt { get; set; }
+        public string PasswordHash { get; set; }
+
+        [MemberNotNull(nameof(Salt), nameof(PasswordHash))]
+        public void SetPassword(string password)
+        {
+            Salt = GenerateRandomSalt();
+            PasswordHash = CalculateHash(password, Salt);
+        }
+
+        public bool CheckPassword(string password) => PasswordHash == CalculateHash(password, Salt);
+        /// <summary>
+        /// Generates a random number with the given length of bits.
+        /// </summary>
+        /// <param name="length">Default: 128 bits (16 Bytes)</param>
+        /// <returns>A base64 encoded string from the byte array.</returns>
+        private string GenerateRandomSalt(int length = 128)
+        {
+            byte[] salt = new byte[length / 8];
+            using (System.Security.Cryptography.RandomNumberGenerator rnd =
+                System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                rnd.GetBytes(salt);
+            }
+            return Convert.ToBase64String(salt);
+        }
+
+        /// <summary>
+        /// Calculates a HMACSHA256 hash value with a given salt.
+        /// <returns>Base64 encoded hash.</returns>
+        private string CalculateHash(string password, string salt)
+        {
+            byte[] saltBytes = Convert.FromBase64String(salt);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            System.Security.Cryptography.HMACSHA256 myHash =
+                new System.Security.Cryptography.HMACSHA256(saltBytes);
+
+            byte[] hashedData = myHash.ComputeHash(passwordBytes);
+
+            // Das Bytearray wird als Hexstring zurückgegeben.
+            return Convert.ToBase64String(hashedData);
+        }
     }
 }
