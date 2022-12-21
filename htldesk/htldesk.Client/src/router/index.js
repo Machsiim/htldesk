59ed651d7d701d3c558827c164af6a3f552eb42c
Grosserzbischof <eric.a.fischer@icloud.com>
2022-12-21 13:51:56 +0100

    Commit 20221221

--- htldesk/htldesk.Client/src/router/index.js
+++ htldesk/htldesk.Client/src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouterView } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,16 +25,10 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/RegisterView.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
     }
+    
   ]
 })
 
 export default router
+
--- htldesk/htldesk.Client/src/views/RegisterView.vue
+++ htldesk/htldesk.Client/src/views/RegisterView.vue
@@ -1,63 +1,95 @@
-<script setup>
-import axios from 'axios';
-</script>
 
 <template>
-    <div>
-      <form @submit.prevent="register">
-        <label>
-          Email:
-          <input v-model="email" type="email" required />
-        </label>
-        <br>
-        <label>
-          Username:
-          <input v-model="username" type="username" required />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input v-model="password" type="password" required />
-        </label>
-        <br />
-        <label>
-          Confirm Password:
-          <input v-model="passwordConfirmation" type="password" required />
-        </label>
-        <br />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  </template>
+  <div class="register">
+    <h1 id="reg_form">Registration Form</h1>
+    <form @submit.prevent="onSubmit">
+  <label for="username">Username:</label>
+  <input v-model="form.username" type="text" id="username" name="username" required>
+  <br>
+  <small class="error"></small>
+  <label for="email">Email:</label>
+  <input v-model="form.email" type="text" id="email" name="email" required>
+  <br>
+  <small class="error"></small>
+  <label for="password">Password:</label>
+  <input v-model="form.password" type="password" id="password" name="password" required>
+  <br>
+  <small class="error"></small>
+  <input type="submit" value="Submit">
+  <p id="success"></p>
+</form> 
+</div>
+</template>
+
+<script>
+function validateForm() {
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const errorNodes = document.querySelectorAll('.error');
+const success = document.getElementById('success');
+clearErrors();
+
+
+let errorFlag = false;
+if(username.value.length < 3) {
+  errorNodes[0].innerText = 'Username must be at least 3 characters long';
+  username.classList.add('error-border');
+  errorFlag = true;
   
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        username: '',
-        password: '',
-        passwordConfirmation: ''
-      }
-    },
-    methods: {
-      async register() {
-        if (this.password !== this.passwordConfirmation) {
-          this.errorMessage = 'Passwords do not match'
-          return
-        }
-        try {
-          await axios.post('https://localhost:5001/api/users', {
-            email: this.email,
-            username: this.username,
-            password: this.password
-          })
-        } catch (error) {
-          console.error(error)
-          // Display an error message to the user
-          this.errorMessage = 'Error creating user'
-        }
-      }
+}
+if(!emailIsValid(email.value)) {
+  errorNodes[1].innerText = 'Email is not valid';
+  email.classList.add('error-border');
+  errorFlag = true;
+}
+if(password.value.length < 8) {
+  errorNodes[2].innerText = 'Password must be at least 8 characters long';
+  password.classList.add('error-border');
+  errorFlag = true;
+}
+return !errorFlag;
+}
+function clearErrors() {
+const errorNodes = document.querySelectorAll('.error');
+errorNodes.forEach(node => node.innerText = '');
+errorNodes.forEach(node => node.classList.remove('error-border'));
+}
+function emailIsValid (email) {
+return /\S+@\S+\.\S+/.test(email);
+}
+import axios from 'axios';
+export default {
+data() {
+  return {
+    form: {
+      username: '',
+      email: '',
+      password:''
     }
   }
-  </script>
+},
+methods: {
+  onSubmit(event) {
+    event.preventDefault();
+    if(validateForm()) {
+      
+    
+    // Submit the form
+    axios.post('https://localhost:5001/api/users', {
+      username: this.form.username,
+      email: this.form.email,
+      password: this.form.password
+    }) .then(response => {
+      console.log(response);
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+}
+}
+};
+</script>
+  
