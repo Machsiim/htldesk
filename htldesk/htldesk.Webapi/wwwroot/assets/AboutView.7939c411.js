6e89530e37be6ce8d6ddf6904d42de3eadfcae45
Machsiim <machsimbs@gmail.com>
2023-01-13 10:10:05 +0100

    Commit 20230113

---
+++ htldesk/htldesk.Webapi/wwwroot/assets/AboutView.7939c411.js
@@ -0,0 +1,1 @@
+import{_ as o,o as c,c as t,p as s,a as _,b as a}from"./index.d4064934.js";const d={},n=e=>(s("data-v-192b11dc"),e=e(),_(),e),p={class:"about"},i=n(()=>a("h1",null,"Hallo ich bin die about page",-1)),r=[i];function l(e,u){return c(),t("div",p,r)}const h=o(d,[["render",l],["__scopeId","data-v-192b11dc"]]);export{h as default};
