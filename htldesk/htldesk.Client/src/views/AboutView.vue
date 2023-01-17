788943d3b8e0b0b9edd0956549b088aaf3ba2ba7
Say My Name <mohamadaldulemi@gmail.com>
2023-01-17 13:26:58 +0100

    Frontend

--- htldesk/htldesk.Client/src/views/AboutView.vue
+++ htldesk/htldesk.Client/src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
-    <h1 class=""> InParis</h1>
+    <h1 class="">InParis</h1>
     <main>
-      
       <p>
-
-        Die Webapplikation hilft Schülern leichter zu arbeiten und einen Übersicht zu haben. Die Ap
+        Die Webapplikation hilft Schülern leichter zu arbeiten und einen
+        Übersicht zu haben. Die Applikation wurde von 5 Schülern <br> der Schule Htl
+        Spengergasse in Wien 1050 Österreich.
       </p>
     </main>
   </div>
